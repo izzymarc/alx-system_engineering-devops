295433c4c7705a02a3b516bfab4318b49f65d915
Ezekiel Gwamna <izzymarc@gmail.com>
2024-04-20 07:06:28 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Ensure pip3 is available
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install the correct version of Werkzeug
+package { 'Werkzeug':
+  ensure   => 'appropriate_version',  # replace appropriate_version with the correct version
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install Flask using pip3, specifying the exact version
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => [Package['python3-pip'], Package['Werkzeug']],  # ensure Werkzeug is installed before Flask
 }
